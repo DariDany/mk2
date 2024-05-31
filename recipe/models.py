ffb42491d8895fad0bfd3ad02fcb805c1240edf0
DariDany <daridanyi70405@gmail.com>
2024-05-31 11:34:17 +0300

    added recipe model

--- recipe/models.py
+++ recipe/models.py
@@ -1,17 +1,21 @@
 from django.db import models
 
 
-class Recipe(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.TextField()
-    created_at = models.DateTimeField()
-
-    def __str__(self):
-        return self.name
-
-
 class Category(models.Model):
     name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    instructions = models.TextField()
+    ingredients = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+
+    def __str__(self):
+        return self.title
