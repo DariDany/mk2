d0e75cf49f3f51fd0ec3b885f59c26016a5e805a
DariDany <daridanyi70405@gmail.com>
2024-05-31 11:25:51 +0300

    added category model

--- recipe/models.py
+++ recipe/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 
+
 class Recipe(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     created_at = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
